4f408658fa9be08e36a90f72db513fd98773975e
Igor <igorancemzik@gmail.com>
2023-08-17 22:20:59 +0500

    fix

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -4,18 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.model.Car;
 import web.service.CarServiceImpl;
-
-import java.util.Arrays;
-import java.util.List;
 
 @Controller
 public class CarController {
     private final CarServiceImpl carService;
-    private static List<Car> carList = Arrays.asList(new Car("BMW", "Black", 5),
-            new Car("Lada", "Red", 10), new Car("Audi", "White", 7),
-            new Car("Kia", "Blue", 1), new Car("Ford", "Grey", 2));
 
     public CarController(CarServiceImpl carService) {
         this.carService = carService;
@@ -23,7 +16,7 @@ public class CarController {
 
     @GetMapping("/cars")
     public String carPage(@RequestParam(value = "count", defaultValue = "5") int count, Model model) {
-        model.addAttribute("cars", carService.getCars(carList, count));
+        model.addAttribute("cars", carService.getCars(count));
         return "cars";
     }
 }
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,15 +1,19 @@
 package web.service;
 
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import web.model.Car;
 
+import java.util.Arrays;
 import java.util.List;
 
-@Component
+@Service
 public class CarServiceImpl implements CarService {
+    private List<Car> carList = Arrays.asList(new Car("BMW", "Black", 5),
+            new Car("Lada", "Red", 10), new Car("Audi", "White", 7),
+            new Car("Kia", "Blue", 1), new Car("Ford", "Grey", 2));
 
     @Override
-    public List<Car> getCars(List<Car> carList, int count) {
+    public List<Car> getCars(int count) {
         return carList.stream().limit(count).toList();
     }
 }
